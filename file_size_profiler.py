bedfc872c7780567621e25ace22c2ff1e81d3f84
Sebastian Smith <sebastian.smith@gmail.com>
2015-10-12 17:48:16 -0700

    output to stdout rather than histogram

--- file_size_profiler.py
+++ file_size_profiler.py
@@ -9,13 +9,14 @@ import os
 def main():
 	parser = OptionParser()
 	parser.add_option('-d', '--directory', dest='dir', action='store', help='top-level directory to profile')
-	parser.add_option('-p', '--plot', dest='plot', action='store', default='file_size_histgram.png', help='file to write histogram')
 
 	option, args = parser.parse_args()
 
 	file_sizes = get_file_sizes(option.dir)
 	hist, edges = bin_files_by_transact_size(file_sizes)
-	plot_to_file(option.plot, hist, edges)
+	print json.dumps({ 'hist': hist.tolist(),
+			'edges': edges.tolist()
+		})
 
 def get_file_sizes(dir):
 	file_sizes = []
@@ -31,11 +32,6 @@ def bin_files_by_transact_size(file_sizes):
 	mb_scalar = 1024.0 ** 2
 	bins = [2**x / mb_scalar for x in range(12, 50)]
 	return np.histogram(map(lambda x: x / mb_scalar, file_sizes), bins)
-
-def plot_to_file(file_name, hist, edges):
-	plt.bar(edges[:-1], hist, edgecolor='red')
-	#plt.xlim(min(edges), max(edges))
-	plt.savefig(file_name)
 
 if __name__ == '__main__':
 	main()
